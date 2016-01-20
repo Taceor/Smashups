a6b144455c36e5afa471d22b8023cc93ade1e645
Taceor <mymailthatis@gmail.com>
2016-01-20 09:39:34 -0700

    Live updates for Upvote/Delete

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -2,6 +2,9 @@ $(document).ready( function() {
 	$('a.upvote').click(function() {
 		var user_nickname = g_user_nickname;
 		var sugg_id = $(this).parent().find(".sugg_id").html();
+		var score = parseInt($(this).parent().find(".score").html());
+		score += 1;
+		$(this).parent().find(".score").text( score );
 		$.post("/_upvote", {
 			user_nickname: user_nickname,
 			sugg_id: sugg_id
@@ -10,6 +13,7 @@ $(document).ready( function() {
 	$('a.delete').click(function() {
 		var user_nickname = g_user_nickname;
 		var sugg_id = $(this).parent().find(".sugg_id").html();
+		$(this).parent().hide();
 		$.post("/_delete_suggestion", {
 			user_nickname: user_nickname,
 			sugg_id: sugg_id
